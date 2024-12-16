268be10a8bcb0a88ac79669057e617093a372e99
Mahendra-Kumawat <maheekumawat9@gmail.com>
2024-12-16 17:26:59 +0530

    update code

---
+++ README.md
@@ -0,0 +1,40 @@
+# 🚀 Getting Started with Tailwind CSS Project
+Welcome to the Tailwind CSS project! Follow these simple steps to set up and run the project locally. 🌟
+
+🛠️ Installation Guide
+
+1️⃣ Clone the Repository
+Start by cloning the project to your local machine:
+
+```bash
+git clone https://github.com/Mahendra-Kumawat/Tailwind_css.git
+
+```
+
+2️⃣ Navigate to the Project Directory
+
+```bash
+cd Tailwind_css
+```
+
+3️⃣ Install Dependencies
+
+```bash
+npm install
+```
+
+
+2️⃣ Start the Development Server
+Run the development server using:
+
+```bash
+npm run dev
+```
+🎉 You're All Set!
+Now you can open your browser and enjoy working with Tailwind CSS and Vite! Happy coding! 😎
+
+📄 Additional Info
+Documentation: [Tailwind CSS Docs](https://tailwindcss.com/docs)
+Vite Docs: [Vite Official Guide](https://vite.dev/guide)
+Feel free to customize the project and build something amazing! 🚀
+
---
+++ script.js
@@ -0,0 +1,118 @@
+console.log("hello welcome");
+
+
+// menu bar (hemburger icon for mobile screen)
+const menuBar = document.querySelector("#menu-bar");
+
+
+
+// navigation bar dilog for mobile
+const navDilog = document.querySelector("#nav-dilog");
+
+
+// close menu icon 
+const closeMenuButton = document.querySelector("#close-menu-button");
+
+// all faqs items
+const  faqsItem = document.querySelectorAll(".faqs-item");
+
+
+// default navigation bar
+const defaultHeader = document.querySelector("#fixed-header");
+
+let inititaltranslateLeftToRight = -700
+let inititaltranslateRightToLeft = 0
+
+menuBar.addEventListener("click", () => {
+  navDilog.classList.toggle("hidden");
+});
+
+closeMenuButton.addEventListener("click", () => {
+  navDilog.classList.toggle("hidden");
+});
+
+
+
+// intersection observer function
+// this function will run when the element is in the view port
+// 
+const setupintersectinObserver = (element, leftTORight, speed) => {
+  const observer = new IntersectionObserver((entries) => {
+    const isIntercepting = entries[0].isIntersecting;
+    if (isIntercepting) {
+      document.addEventListener("scroll", scrollHandler);
+    }
+    else{
+      document.removeEventListener("scroll", scrollHandler);
+    }
+  });
+
+  observer.observe(element);
+
+  // scroll handler function to update the translate value of the element
+  const scrollHandler = () => {
+    const translateX =
+      (window.innerHeight - element.getBoundingClientRect().top) * speed;
+    console.log(translateX);
+    let totalTranslate = 0;
+    if(leftTORight){
+      totalTranslate = inititaltranslateLeftToRight + translateX
+    }else{
+      totalTranslate = inititaltranslateRightToLeft - translateX
+    }
+    element.style.transform = `translateX(${totalTranslate}px)`;  
+  };
+};
+
+// fetch all the lines 
+const line1 = document.querySelector("#line1");
+const line2 = document.querySelector("#line2");
+const line3 = document.querySelector("#line3");
+const line4 = document.querySelector("#line4")
+
+
+// calling the intersection observer
+setupintersectinObserver(line1, true, 0.2);
+setupintersectinObserver(line2, false, 0.5);
+setupintersectinObserver(line3, true, 0.7);
+setupintersectinObserver(line4, false, 0.7);
+
+
+
+
+
+// expand and collapse the faqs items 
+
+faqsItem.forEach((item) => {
+  item.addEventListener("click", (event) => {
+    const i = event.currentTarget.querySelector("i")
+    i.classList.toggle("-rotate-180")
+    event.currentTarget.nextElementSibling.classList.toggle("hidden")
+  })
+}
+)
+
+
+
+
+
+// this is the custom scroll event
+const awesomeNavbar = document.querySelector("#awesome-navbar");
+
+document.addEventListener("scroll", () => {
+  console.log("inside the scroll event")
+  const scrollY = window.scrollY;
+  console.log(scrollY)
+  if (scrollY > 400) {
+    awesomeNavbar.classList.remove("hidden");
+    awesomeNavbar.classList.add("flex");
+    console.log("scrooled 400")
+    defaultHeader.classList.add("hidden");
+  }
+  else {
+    awesomeNavbar.classList.remove("flex");
+    awesomeNavbar.classList.add("hidden");
+    console.log("not scrooled 400")
+    defaultHeader.classList.remove("hidden");
+  }
+});
